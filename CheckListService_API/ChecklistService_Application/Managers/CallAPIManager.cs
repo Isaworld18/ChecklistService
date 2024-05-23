6c8e28ffe70d4ec8bce9b1e56649c1ce8a0721e0
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2024-05-23 17:27:13 +0200

    CallAPIManager class implementatiion

---
+++ CheckListService_API/ChecklistService_Application/Managers/CallAPIManager.cs
@@ -0,0 +1,34 @@
+﻿namespace ChecklistService_Application.Managers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http.Headers;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+
+    public static class CallAPIManager
+    {
+        public static async Task<List<T>?> GetBanksData<T>(string apiSource, string path, T resultDataType)
+                                                           where T : IConvertible
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri(apiSource)
+            };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            List<T>? data = [];
+
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadFromJsonAsync<List<T>>();
+            }
+
+            return data;
+        }
+    }
+}
--- CheckListService_API/ChecklistService_Domain/Models/Work.cs
+++ CheckListService_API/ChecklistService_Domain/Models/Work.cs
@@ -1,11 +1,14 @@
 ﻿namespace ChecklistService_Domain.Models;
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public partial class Work
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(50)]
     public string Description { get; set; } = null!;
 
     public bool Done { get; set; }
